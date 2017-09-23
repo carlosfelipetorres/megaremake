748961532028cc463343510d982f13f238f70ddb
carlosfelipetorres <ctorres@tappsi.co>
2017-09-23 08:09:08 -0500

    first scene

---
+++ megaremake/Assets/Scripts/AttractedToObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractedToObject : MonoBehaviour {
+
+	public GameObject attractedTo;
+	public float strengthOfAttraction = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		//gameObject.GetComponent<Rigidbody>().AddForce(strengthOfAttraction * new Vector3(Random.Range(800, 1700) ,0,0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 direction = attractedTo.transform.position - transform.position;
+		Vector3 direction = new Vector3(0,0,0) - transform.position;
+		gameObject.GetComponent<Rigidbody>().AddForce(strengthOfAttraction * direction);
+
+	}
+}
---
+++ megaremake/Assets/Scripts/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+	public GameObject cube;
+	int i = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		i++;
+		if(i%100 == 0){
+			Instantiate (cube, new Vector3 (Random.Range (-20, 20), 0, Random.Range (-20, 20)), Quaternion.identity);
+		}
+	}
+}
