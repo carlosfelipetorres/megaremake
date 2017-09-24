c644b6d63e0efa51808e4231f6542dca7e4017eb
Jose Aponte <joseapontec@gmail.com>
2017-09-23 23:04:51 -0500

    Fixed some scripts and reduce the numbers of scripst (reorganized code) Added effect explosion when collide enemy with player Replace the Enemy sprite with 3d object and add Chaser script to follow the player Generate random position por enemy generator Added effect when user destroy an enemy

---
+++ megaremake/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public GameObject explosion;
+
+	void OnCollisionEnter (Collision myCol)
+	{
+		if (myCol.gameObject.tag == "Enemy")
+		{
+			GameObject tmp = Instantiate (explosion, new Vector3 (0, 0, 0), Quaternion.identity);
+			Destroy (tmp, 4);
+		}
+		Destroy (myCol.gameObject);
+	}
+}
